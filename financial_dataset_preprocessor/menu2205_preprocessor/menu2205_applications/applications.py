6e7d335d86283adda14e39e73ec0bcc3c2b9e44a
JuneYoung <nietsnie1@gmail.com>
2025-03-05 09:13:56 +0900

    feat: add asset search functions by keyword(s)
    - Add new search functions to find assets by keyword
    - Add multiple keywords search capability
    - Update version to 0.1.8
    - Add .windsurfrules to .gitignore

--- financial_dataset_preprocessor/menu2205_preprocessor/menu2205_applications/applications.py
+++ financial_dataset_preprocessor/menu2205_preprocessor/menu2205_applications/applications.py
@@ -96,3 +96,25 @@ def get_grouped_dfs_of_menu2205_by_fund(fund_code, col, date_ref=None):
 # ['국내주식', '국내채권', 'REPO 매수', 'REPO', '국내현금', '국내선물', '국내수익증권',
 #        '국내수익증권(ETF)', '외화주식', '외화현금성', '외화스왑', '기타', '기타(총계정원장)', '총계정원장',
 #        '차입금']
+
+def search_asset_by_keyword(keyword, date_ref=None):
+    df = get_preprocessed_menu2205_snapshot(date_ref=date_ref)
+    df = df[df['종목명'].str.contains(keyword)]
+    return df
+
+def search_asset_by_keyword_in_fund(keyword, fund_code, date_ref=None):
+    df = get_preprocessed_menu2205(fund_code=fund_code, date_ref=date_ref)
+    df = df[df['종목명'].str.contains(keyword)]
+    return df
+
+def search_asset_including_keywords(keywords, date_ref=None):
+    df = get_preprocessed_menu2205_snapshot(date_ref=date_ref)
+    reg_keywords = '|'.join(keywords)
+    df = df[df['종목명'].str.contains(reg_keywords)]
+    return df
+
+def search_asset_including_keywords_in_fund(keywords, fund_code, date_ref=None):
+    df = get_preprocessed_menu2205(fund_code=fund_code, date_ref=date_ref)
+    reg_keywords = '|'.join(keywords)
+    df = df[df['종목명'].str.contains(reg_keywords)]
+    return df
