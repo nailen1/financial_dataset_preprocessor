9ac7afc4d9f15160518da0e54872b53aad0e0dac
JuneYoung <nietsnie1@gmail.com>
2025-03-07 13:17:58 +0900

    feat: add snapshot data retrieval for menu2160
    - Add get_preprocessed_menu2160_snapshot() to retrieve preprocessed snapshot data
    - Add alias map_fund_code_to_preprocessed_menu2160_snapshot
    - Update version to 0.1.11

--- financial_dataset_preprocessor/menu2160_preprocessor/menu2160.py
+++ financial_dataset_preprocessor/menu2160_preprocessor/menu2160.py
@@ -1,5 +1,5 @@
 from pandas import DataFrame
-from financial_dataset_loader import load_menu2160
+from financial_dataset_loader import load_menu2160, load_menu2160_snapshot
 from .menu2160_column_preprocessor import get_preprocessed_column_names_menu2160
 from .menu2160_value_preprocessor import preprocess_cols_num_menu2160, preprocess_cols_fund_code_menu2160
 from ..row_preprocessor import drop_non_indexed_rows
@@ -14,9 +14,12 @@ def preprocess_raw_menu2160(menu2160: DataFrame) -> DataFrame:
         .pipe(drop_non_indexed_rows)
     )
 
-
 def get_preprocessed_menu2160(fund_code):
     return preprocess_raw_menu2160(load_menu2160(fund_code))
 
+def get_preprocessed_menu2160_snapshot(fund_code):
+    return preprocess_raw_menu2160(load_menu2160_snapshot(fund_code))
+
 map_raw_to_preprocessed_menu2160 = preprocess_raw_menu2160
 map_fund_code_to_preprocessed_menu2160 = get_preprocessed_menu2160
+map_fund_code_to_preprocessed_menu2160_snapshot = get_preprocessed_menu2160_snapshot
