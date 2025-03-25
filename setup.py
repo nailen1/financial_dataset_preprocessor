8b00388a76cd97559a69a83b0993987fe8962c43
JuneYoung <nietsnie1@gmail.com>
2025-03-25 17:08:07 +0900

    chore: bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='financial_dataset_preprocessor',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     install_requires=[
         'pandas',
